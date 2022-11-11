7e50ce24c726a340447d315de18290c94ea1aedf
Daniela P. Riesgo <daniela@evolve.xyz>
2022-11-11 12:56:58 -0300

    Simulate MainView without Banner

--- DeepLinkTest/DeepLinkTest/MainView.swift
+++ DeepLinkTest/DeepLinkTest/MainView.swift
@@ -10,24 +10,18 @@ import SwiftUI
 struct MainView: View {
 
     @EnvironmentObject var deepLink: DeepLinkViewModel
-    @State private var currentTab: Int = 0
-    @State private var dp1 = DeepLinkViewModel()
-    @State private var dp2 = DeepLinkViewModel()
+    @EnvironmentObject var viewModel: ViewModel
 
     var body: some View {
-        TabView(selection: $currentTab) {
+        TabView(selection: $viewModel.currentTab) {
             NavigationView {
-                Tab1View().environmentObject(dp1)
+                Tab1View().environmentObject(viewModel.dp1)
             }.tabItem { Text("Tab1") }.tag(0)
             NavigationView {
-                Tab2View().environmentObject(dp2)
+                Tab2View().environmentObject(viewModel.dp2)
             }.tabItem { Text("Tab2") }.tag(1)
         }.onChange(of: deepLink) { newValue in
-            switch currentTab {
-            case 0: dp1 = newValue
-            case 1: dp2 = newValue
-            default: break
-            }
+            viewModel.generalDeepLink = newValue
         }
     }
 }
@@ -35,6 +29,24 @@ struct MainView: View {
 extension MainView {
 
     class ViewModel: ObservableObject {
+
+        @Published var currentTab: Int = 0
+        @Published var dp1 = DeepLinkViewModel()
+        @Published var dp2 = DeepLinkViewModel()
+
+        var generalDeepLink: DeepLinkViewModel {
+            didSet {
+                switch currentTab {
+                case 0: dp1 = generalDeepLink
+                case 1: dp2 = generalDeepLink
+                default: break
+                }
+            }
+        }
+
+        init() {
+            generalDeepLink = DeepLinkViewModel()
+        }
 
     }
 
